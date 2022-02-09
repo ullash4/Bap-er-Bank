17bee5a0f47a16e5b63dbe1ba3491e8cb4ae2c46
ullash4 <programmerullash@gmail.com>
2022-02-09 23:54:11 +0600

    third

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.getElementById('submit-btn').addEventListener('click', function (){
+    // Get user email 
+    const showEmail = document.getElementById('user-mail');
+    const userEmail = showEmail.value;
+    // Get user passwords 
+    const showPass = document.getElementById('user-pass');
+    const userPass = showPass.value;
+    // If else conditions 
+    if(userEmail=='sontan@baap.com' && userPass=='ullash'){
+        window.location.href='bank.html';
+    }
+    else{
+        alert('who are you')
+    }
+})
